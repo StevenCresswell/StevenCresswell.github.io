15126cab6c2bf6af102d31c63f9be7c0a4695f3e
Steven Cresswell <stevencresswell21@gmail.com>
2024-02-21 20:11:48 +0000

    added to skills section, formatted the footer and updated the contacts page

---
+++ js/website.js
@@ -0,0 +1,60 @@
+
+/*const skillsContainer = document.getElementById("skills-container");
+
+const showSkill = (event) => {
+    if (event.target === document.getElementById("icon-1")) {
+        document.getElementById("skill-1").style.display = "block"
+    }
+    else if (event.target === document.getElementById("icon-2")) {
+        document.getElementById("skill-2").style.display = "block"
+    }
+    else if (event.target === document.getElementById("icon-3")) {
+        document.getElementById("skill-3").style.display = "block"
+    }
+    else if (event.target === document.getElementById("icon-4")) {
+        document.getElementById("skill-4").style.display = "block"
+    }
+    else if (event.target === document.getElementById("icon-5")) {
+        document.getElementById("skill-5").style.display = "block"
+    }
+    else if (event.target === document.getElementById("icon-6")) {
+        document.getElementById("skill-6").style.display = "block"
+    }
+    else if (event.target === document.getElementById("icon-7")) {
+        document.getElementById("skill-7").style.display = "block"
+    }
+}
+
+
+const hideSkill = (event) => {
+    if (event.target === document.getElementById("skill-1")) {
+        document.getElementById("skill-1").style.display = "none"
+    }
+    else if (event.target === document.getElementById("skill-2")) {
+        document.getElementById("skill-2").style.display = "none"
+    }
+    else if (event.target === document.getElementById("skill-3")) {
+        document.getElementById("skill-3").style.display = "none"
+    }
+    else if (event.target === document.getElementById("skill-4")) {
+        document.getElementById("skill-4").style.display = "none"
+    }
+    else if (event.target === document.getElementById("skill-5")) {
+        document.getElementById("skill-5").style.display = "none"
+    }
+    else if (event.target === document.getElementById("skill-6")) {
+        document.getElementById("skill-6").style.display = "none"
+    }
+    else if (event.target === document.getElementById("skill-7")) {
+        document.getElementById("skill-7").style.display = "none"
+    }
+} 
+
+skillsContainer.addEventListener("mouseover", (event) => {
+        setTimeout(() => showSkill(event), 500);
+});
+
+skillsContainer.addEventListener("mouseout", (event) => {
+        setTimeout(() => hideSkill(event), 500);
+});
+*/
